34f3246b500df3c2b895dabcb5e521eeee14969b
Imabanana80 <imabanana80.hallo@gmail.com>
2023-03-04 22:26:03 +0800

    removed useless comment

--- src/main/java/com/Imabanana80/mcsrcapes/Main.java
+++ src/main/java/com/Imabanana80/mcsrcapes/Main.java
@@ -6,16 +6,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Starting...");
         Config.createDefaultConfig();
-/*
-        File folder = new File("C:/Users/imaba/Desktop/Portable_Programs/PrismLauncher");
-        File[] listOfFiles = folder.listFiles();
-
-
-        for (File file : listOfFiles) {
-
-            System.out.println(file.getName());
-        }
-*/
         }
 
 }
